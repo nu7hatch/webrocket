5adcf5a9863decc4c1c03eaac267720480f4e299
Chris Kowalik <chris@nu7hat.ch>
2011-09-02 04:38:22 -0300

    fixed connection problems

--- main.go
+++ main.go
@@ -5,35 +5,40 @@ import (
 	"websocket"
 	"http"
 	"log"
-	"json"
 	"os"
-	"bytes"
 	)
 
-const (
-	HubAlreadyRegisteredErr = 1
-	)
+type DataMap map[string]string
 
-type Event struct {
-	Event string
-	Body  string
-	Data  map[string]string
+type NamedEvent struct {
+	Event   string
 }
 
-type Error struct {
-	code int
+type ChanneledEvent struct {
+	Event   string
+	Channel string
+}
+
+type DataEvent struct {
+	Event   string
+	Channel string
+	Data    DataMap
+}
+
+type ErrorEvent struct {
+	Error   string
 }
 
 type Channel struct {
 	Name     string
 
-	backend  chan *Event
+	//backend  chan *Event
 	ownerHub *Hub
 }
 
 func newChannel(hub *Hub, name string) *Channel {
 	ch := &Channel{Name: name, ownerHub: hub}
-	ch.backend = make(chan *Event)
+	//ch.backend = make(chan *Event)
 	return ch
 }
 
@@ -46,41 +51,6 @@ type Hub struct {
 	registered bool
 }
 
-type Conn struct {
-	Ws  *websocket.Conn
-	
-	out *json.Encoder
-}
-
-func newConn(ws *websocket.Conn) *Conn {
-	c := &Conn{Ws: ws}
-	c.out = json.NewEncoder(ws)
-	return c
-}
-
-func (c *Conn) Read(e *Event) os.Error {
-	var buf bytes.Buffer
-	msg := make([]byte, 1024)
-
-	for {
-		n, err := c.Ws.Read(msg)
-		buf.Write(msg[:n])
-
-		if n < 1024 || err == os.EOF {
-			break
-		}
-		if err != nil {
-			return err
-		}
-	}
-	
-	return json.Unmarshal(buf.Bytes(), e)
-}
-
-func (c *Conn) Write(e *Event) os.Error {
-	return c.out.Encode(e)
-}
-
 func NewHub(name, pass string) *Hub {
 	h := &Hub{Name: name, Passphrase: pass}
 	h.Handler = func(ws *websocket.Conn) { h.serve(ws) }
@@ -88,62 +58,80 @@ func NewHub(name, pass string) *Hub {
 	return h
 }
 
-func (h *Hub) setup(conn *Conn) bool {
-	if err := conn.Write(&Event{Event: "connected"}); err != nil {
-		log.Printf("Conn error: %s!\n", err.String())
-		return false
+func (h *Hub) setup(ws *websocket.Conn) os.Error {
+	err := websocket.JSON.Send(ws, NamedEvent{"connected"})
+	if err != nil {
+		log.Printf("Connection error: %s!\n", err.String())
+		return err
 	}
 	
-	log.Printf("Connected: %s\n", conn.Ws.Origin)
-	return true
+	//log.Printf("Connected: %s\n", ws.RemoteAddr())
+	return nil
 }
 
-func (h *Hub) subscribe(conn *Conn, event *Event) bool {
-	if name, ok := event.Data["channel"]; ok {
-		ch, ok := h.channels[name]
-		
-		if !ok {
-			ch = newChannel(h, name)
-			h.channels[name] = ch
-		}
+func (h *Hub) subscribe(ws *websocket.Conn, event *DataEvent) os.Error {
+	if len(event.Channel) == 0 {
+		err := os.NewError("invalid channel")
+		log.Printf("Subscribe error: %s\n", err.String())
+		websocket.JSON.Send(ws, ErrorEvent{"INVALID_CHANNEL"})
+		return err
+	}
+
+	ch, ok := h.channels[event.Channel]
+	if !ok {
+		ch = newChannel(h, event.Channel)
+		h.channels[event.Channel] = ch
+	}
 	
-		log.Printf("Subscribed %s\n", ch.Name)
-		err := conn.Write(&Event{Event: "subscribed", Data: map[string]string{"channel": ch.Name}})
-		if err == nil { return true }
+	err := websocket.JSON.Send(ws, ChanneledEvent{"subscribed", ch.Name})
+	if err != nil {
+		log.Printf("Subscribe error: %s\n", err.String())
+		return err
 	}
-	return false
+
+	log.Printf("Subscribed: %s\n", ch.Name)
+	return nil
 }
 
-func (h *Hub) unsubscribe(conn *Conn, event *Event) bool {
-	if name, ok := event.Data["channel"]; ok {
-		if _, ok := h.channels[name]; ok {
-			log.Printf("Unsubscribed %s\n", name)
-			err := conn.Write(&Event{Event: "unsubscribed", Data: map[string]string{"channel": name}})
-			if err == nil { return true }
-		}
+func (h *Hub) unsubscribe(ws *websocket.Conn, event *DataEvent) os.Error {
+	ch, ok := h.channels[event.Channel]
+	if !ok {
+		err := os.NewError("invalid channel")
+		log.Printf("Unsubscribe error: %s\n", err.String())
+		websocket.JSON.Send(ws, ErrorEvent{"INVALID_CHANNEL"})
+		return err
 	}
-	return false
+	
+	err := websocket.JSON.Send(ws, ChanneledEvent{"unsubscribed", ch.Name})
+	if err != nil {
+		log.Printf("Unsubscribe error: %s\n", err)
+		return err
+	}
+	
+	log.Printf("Unsubscribed %s\n", ch.Name)
+	return nil
 }
 
 func (h *Hub) serve(ws *websocket.Conn) {
-	conn := newConn(ws)
-	
-	if h.setup(conn) {
+	err := h.setup(ws)
+	if err == nil {
 		for {
-			var event Event
-			err := conn.Read(&event)
-
+			var event DataEvent
+			err = websocket.JSON.Receive(ws, &event)
+			
 			if err == nil {
 				switch event.Event {
 				case "__subscribe__":
-					h.subscribe(conn, &event)
+					h.subscribe(ws, &event)
 				case "__unsubscribe__":
-					h.unsubscribe(conn, &event)
+					h.unsubscribe(ws, &event)
+				default:
+					websocket.JSON.Send(ws, ErrorEvent{"INVALID_COMMAND"})
 				}
 			} else {
-				log.Printf("Event error: %s!\n", err.String())
+				log.Printf("Receive error: %s!\n", err.String())
 			}
-		}
+	    }
 	}
 }
 
@@ -166,9 +154,9 @@ func (s *Server) url() string {
 	return s.Host + ":" + strconv.Uitoa(s.Port)
 }
 
-func (s *Server) RegisterHub(hub *Hub) *Error {
+func (s *Server) RegisterHub(hub *Hub) os.Error {
 	if hub.registered {
-		return &Error{HubAlreadyRegisteredErr}
+		return os.NewError("hub already registered")
 	}
 
 	handler := websocket.Handler(hub.Handler)
@@ -179,15 +167,15 @@ func (s *Server) RegisterHub(hub *Hub) *Error {
 	return nil
 }
 
-func (s *Server) CreateHub(name, pass string) *Error {
+func (s *Server) CreateHub(name, pass string) os.Error {
 	h := NewHub(name, pass)
 	return s.RegisterHub(h)
 }
 
 func (s *Server) Start() {
 	log.Printf("About to listen on ws://%s:%d", s.Host, s.Port)
-	err := s.backend.ListenAndServe()
 
+	err := s.backend.ListenAndServe()
 	if err != nil {
 		log.Fatalf("Can't setup listener - %s\n", err.String())
 	}
