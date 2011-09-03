5edf19527e61a704e687d3da94b6b4f2cff836aa
Chris Kowalik <chris@nu7hat.ch>
2011-09-03 02:28:12 -0300

    added docs

--- webrocket.go
+++ webrocket.go
@@ -1,3 +1,9 @@
+// Copyright 2011 Chris Kowalik (nu7hatch). All rights reserved.
+// Use of this source code is governed by a BSD-style license that
+// can be found in the LICENSE file.
+//
+// Package webrocket implements advanced WebSocket server with custom
+// protocols support. 
 package webrocket
 
 import (
